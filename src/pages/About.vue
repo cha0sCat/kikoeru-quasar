60aca21ce965bd43274b5548d895f753d424ab77
cha0sCat <admin@noob.pw>
2021-03-28 00:57:44 +0800

    feat: 关于本站

---
+++ src/pages/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-page padding>
+    <h4 id="dmca">Contact / DMCA</h4>
+    <p>Please contact asmr.one@protonmail.com</p>
+
+    <h4 id="whoami">我们是谁</h4>
+    <p>我们是普通的音声爱好者，架设网站用于引导小白入坑，方便老司机办事</p>
+
+    <h4 id="contract">联系我们</h4>
+    <p>如果你有任何想法或建议，欢迎联系我们 asmr.one@protonmail.com</p>
+
+
+  </q-page>
+</template>
+
+<script>
+export default {
+name: "About"
+}
+</script>
+
+<style scoped>
+
+</style>
